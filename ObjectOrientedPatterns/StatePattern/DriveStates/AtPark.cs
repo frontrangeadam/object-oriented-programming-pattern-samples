d34d926fe6e8392567173efc13c029de2cb8c5a1
Adam Wilson <awilson@globallearningsystems.com>
2018-09-19 17:43:15 -0400

    Add state pattern project.

---
+++ ObjectOrientedPatterns/StatePattern/DriveStates/AtPark.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StatePattern.Interfaces;
+
+namespace StatePattern.DriveStates
+{
+	public class AtPark : IDriveState
+	{
+		public IDriveState Braking()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDriveState ToGearRatio(int targetGearRatio)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDriveState ToNeutral()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDriveState ToPark() => this;
+
+		public IDriveState ToReverse()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ ObjectOrientedPatterns/StatePattern/Interfaces/IDriveState.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace StatePattern.Interfaces
+{
+	public interface IDriveState
+	{
+		IDriveState Braking();
+		IDriveState ToGearRatio(int targetGearRatio);
+		IDriveState ToNeutral();
+		IDriveState ToPark();
+		IDriveState ToReverse();
+	}
+}
